add1aec19d2f22eb0a9c7fd64c1ee5c12da906a5
aeller <jpns3334444@gmail.com>
2025-12-02 21:40:59 -0700

    remove picsum mal

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,14 +5,6 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "picsum.photos",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.myanimelist.net",
-      },
-      {
-        protocol: "https",
         hostname: "manga-reader-manga-images-418987949764.s3.amazonaws.com",
       },
       {
